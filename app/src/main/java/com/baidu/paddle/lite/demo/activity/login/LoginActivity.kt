15e8bcfd833fffc68457e8e4fc4b636af8183c71
csy-18 <72370324+csy-18@users.noreply.github.com>
2021-07-30 19:58:41 +0800

    7/30/3

---
+++ app/src/main/java/com/baidu/paddle/lite/demo/activity/login/LoginActivity.kt
@@ -0,0 +1,28 @@
+package com.baidu.paddle.lite.demo.activity.login
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.baidu.paddle.lite.demo.activity.camera.CameraActivity
+import com.baidu.paddle.lite.demo.activity.main.SettingActivity
+import com.baidu.paddle.lite.demo.ocr.R
+import com.baidu.paddle.lite.demo.ocr.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.button2.setOnClickListener {
+            startActivity(Intent(this,CameraActivity::class.java))
+        }
+        binding.button3.setOnClickListener {
+            startActivity(Intent(this,SettingActivity::class.java))
+        }
+    }
+}
---
+++ app/src/main/java/com/baidu/paddle/lite/demo/activity/main/SettingActivity.kt
@@ -0,0 +1,48 @@
+package com.baidu.paddle.lite.demo.activity.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.util.Log
+import com.baidu.paddle.lite.demo.network.OdooUtils
+import com.baidu.paddle.lite.demo.ocr.R
+import com.baidu.paddle.lite.demo.ocr.databinding.ActivitySettingBinding
+
+class SettingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySettingBinding
+    private lateinit var work: Handler
+
+    companion object {
+        var TAG = SettingActivity.javaClass.simpleName
+        const val GET_VERSION = 1
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySettingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViews()
+        initHandler()
+    }
+
+    private fun initHandler() {
+        work = object : Handler(mainLooper) {
+            override fun handleMessage(msg: Message) {
+                when (msg.what) {
+                    GET_VERSION -> {
+                        val version = OdooUtils.getVersion()
+                        Log.i(TAG, "initViews: $version")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun initViews() {
+        binding.button4.setOnClickListener {
+            work.sendEmptyMessage(GET_VERSION)
+        }
+    }
+}
